c6a60482caf49f76b62db0c3190acbb2b7d0a65b
Scott Phillips <scotty.phillips@hotmail.com>
2018-11-27 16:13:53 +1100

    Preparing PyPi package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='mitsubishi_echonet',
+    version='0.1.0',
+    author='Scott Phillips',
+    author_email='jrh@example.com',
+    packages=['mitsubishi_echonet'],
+    url='http://pypi.python.org/pypi/mitsubishi_echonet/',
+    license='LICENSE.txt',
+    description='A library for interfacing with Mitsubishi HVAC via the Echonet lite protocol.',
+    long_description=open('README.txt').read(),
+    # install_requires=[
+    #    "Django >= 1.1.1",
+    #    "caldav == 0.1.4",
+    #],
+)
